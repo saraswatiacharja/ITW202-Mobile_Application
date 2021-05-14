9118c9ed877899833f4fe6ab013ad4ad6415da73
saraswatiacharja <12190075.gcit@rub.edu.bt>
2021-05-14 12:11:48 +0600

    labwrkss

--- udp.java
+++ udp.java
@@ -1,20 +1,34 @@
-import java.util.Scanner; 
-
-public class udp
-{ 
-	public static void main(String args[]){
+import java.util.Scanner;
+public class udp{  
+   public static void main(String args[]){
 
 
- 	Scanner scanner = new Scanner(System.in); 
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter Hexadecimal Digits:  ");
+String hexnum = scanner.nextLine();
+scanner.close();
+int len = hexnum.length();
+int n = 4;  
+    int temp = 0, chars = len/n;  
+        //Stores the array of string  
+    String[] equalStr = new String [n];  
 
-	System.out.print("Enter any hexadecimal number: "); 
-	String sourceNum = scanner.nextLine(); 
+for(int i = 0; i < len; i = i+chars) {  
+                //Dividing string in n equal part using substring()  
+                String part = hexnum.substring(i, i+chars);  
+                equalStr[temp] = part;  
+                temp++;  
+            }    
+            int num = Integer.parseInt(equalStr[0],16);
+            int num1 = Integer.parseInt(equalStr[1],16);
+int num2 = Integer.parseInt(equalStr[2],16);
+int num3 = num2-8;
 
-	scanner.close(); 
 
-//converting hex to decimal by passing base 16 
+System.out.println("Source port number is: "+num);
+System.out.println("Destination port number is: "+num1);
+System.out.println("Total length of the user datagram: "+num2);
+System.out.println("length of the data: "+num3);
 
-	int num = Integer.parseInt(sourceNum,16); 
-
-	System.out.println("Source Port Number: "+num); } 
+   }
 }
